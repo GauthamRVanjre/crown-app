ba4327873b538a7b0ddfdd70012eae31c45f7a94
Gautham Vanjre <vanjregautham@gmail.com>
2024-03-03 20:08:39 +0530

    feat: testing shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h1>Hello, testing development branch</h1>
+      <Button>Test</Button>
     </>
   );
 }

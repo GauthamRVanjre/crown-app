89ad708787b3d64f42dbd1811bc82446479bff8e
Gautham Vanjre <vanjregautham@gmail.com>
2024-03-05 21:02:13 +0530

    feat: page UI colors and login form UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import LoginForm from "@/components/LoginForm";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1>Hello, testing development branch</h1>
-      <Button>Test</Button>
+      <div className="flex justify-center items-center">
+        <LoginForm />
+      </div>
     </>
   );
 }

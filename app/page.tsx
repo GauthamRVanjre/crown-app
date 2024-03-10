296d22c9baef0be3d4cc011d0cd11a3bb825225b
Gautham Vanjre <vanjregautham@gmail.com>
2024-03-10 14:51:12 +0530

    feat: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,34 @@
-"use client";
-import LoginCard from "@/components/LoginCard";
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/FA7v3GPzcZf
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
 import Navbar from "@/components/Navbar";
-import { Button } from "@/components/ui/button";
-import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
 
-export default function Home() {
-  const { data, status } = useSession();
-
+export default function Page() {
   return (
     <>
-      <Navbar />
-      <div className="flex justify-center items-center mt-10">
-        <LoginCard />
-      </div>
+      <section className="w-full py-12 md:py-24 h-full">
+        <div className=" container px-4 md:px-6 mt-32">
+          <div className="flex flex-col items-center space-y-4 text-center">
+            <div className="space-y-4">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                Welcome To Crown
+              </h1>
+              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                Stay on top of your Investments
+              </p>
+            </div>
+            <Link
+              className="inline-flex h-9 items-center justify-center rounded-md border  bg-black px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-amber-300 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+              href="/Login"
+            >
+              Get Started with Next.js
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 }

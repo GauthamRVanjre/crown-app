288f860955a4299a782284cd967af64ddb30e55c
Gautham Vanjre <vanjregautham@gmail.com>
2024-04-19 20:06:02 +0530

    feat: user detail form

--- components/Profile/AddUserDetailsForm.tsx
+++ components/Profile/AddUserDetailsForm.tsx
@@ -10,6 +10,14 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Textarea } from "@/components/ui/textarea";
@@ -34,7 +42,7 @@ const AddUserDetailsForm = () => {
     defaultValues: {
       brokerName: "",
       phoneNumber: 0,
-      investmentGoal: "",
+      investmentGoal: 0,
       riskTakingCapacity: "",
     },
   });
@@ -42,120 +50,139 @@ const AddUserDetailsForm = () => {
   const onFinish = async (
     values: z.infer<typeof AddUserDetailFormValidation>
   ) => {
-    setIsLoading(true);
-
-    // console.log("values", values);
-    try {
-      const response = await fetch(`/api/users`, {
-        method: "POST",
-        body: JSON.stringify(values),
-      });
-
-      if (response.status === 200) {
-        queryClient.invalidateQueries({ queryKey: ["users"] });
-        toast.success("user created successfully");
-      } else {
-        toast.error("something went wrong! try again");
-      }
-    } catch (error) {
-      console.log("something went wrong");
-    } finally {
-      setIsLoading(false);
-      form.reset();
-    }
+    // setIsLoading(true);
+    console.log("values", values);
+    // try {
+    //   const response = await fetch(`/api/users`, {
+    //     method: "POST",
+    //     body: JSON.stringify(values),
+    //   });
+    //   if (response.status === 200) {
+    //     queryClient.invalidateQueries({ queryKey: ["users"] });
+    //     toast.success("user created successfully");
+    //   } else {
+    //     toast.error("something went wrong! try again");
+    //   }
+    // } catch (error) {
+    //   console.log("something went wrong");
+    // } finally {
+    //   setIsLoading(false);
+    //   form.reset();
+    // }
   };
   return (
     <>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onFinish)} className="space-y-8">
-          <ScrollArea className="h-[400px]">
-            <FormField
-              control={form.control}
-              name="brokerName"
-              render={({ field }) => (
-                <FormItem className="mt-8 w-[400px]">
-                  <FormLabel>Broker Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={isLoading}
-                      className="glass rounded-2xl"
-                      type="text"
-                      placeholder="Enter Broker Name"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="brokerName"
+            render={({ field }) => (
+              <FormItem className="mt-8 w-[400px]">
+                <FormLabel>Broker Name</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    className="glass rounded-2xl"
+                    type="text"
+                    placeholder="Enter Broker Name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={form.control}
-              name="phoneNumber"
-              render={({ field }) => (
-                <FormItem className="mt-8 w-[400px]">
-                  <FormLabel>Phone Number</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={isLoading}
-                      className="glass rounded-2xl"
-                      type="number"
-                      placeholder="Enter Phone Number"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem className="mt-8 w-[400px]">
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter Phone Number"
+                    {...field}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        field.onChange(0);
+                      } else if (Number.isNaN(Number(value))) {
+                        field.onChange(0);
+                      } else {
+                        field.onChange(Number(value));
+                      }
+                    }}
+                    disabled={isLoading}
+                    className="glass rounded-2xl"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={form.control}
-              name="investmentGoal"
-              render={({ field }) => (
-                <FormItem className="mt-8 w-[400px]">
-                  <FormLabel>Investment Goal</FormLabel>
-                  <FormControl>
-                    <Input
-                      disabled={isLoading}
-                      className="glass rounded-2xl"
-                      type="number"
-                      placeholder="Enter Investment Goal"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="investmentGoal"
+            render={({ field }) => (
+              <FormItem className="mt-8 w-[400px]">
+                <FormLabel>Investment Goal</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter Investment Goal"
+                    {...field}
+                    disabled={isLoading}
+                    className="glass rounded-2xl"
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (value === "") {
+                        field.onChange(0);
+                      } else if (Number.isNaN(Number(value))) {
+                        field.onChange(0);
+                      } else {
+                        field.onChange(Number(value));
+                      }
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={form.control}
-              name="riskTakingCapacity"
-              render={({ field }) => (
-                <FormItem className="mt-8 w-[400px]">
-                  <FormLabel>Risk Taking Capacity</FormLabel>
-                  <FormControl>
-                    <select
-                      disabled={isLoading}
-                      className="glass rounded-2xl"
-                      {...field}
-                    >
-                      <option value="">Select</option>
-                      <option value="low">Low</option>
-                      <option value="medium">Medium</option>
-                      <option value="high">High</option>
-                    </select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="riskTakingCapacity"
+            render={({ field }) => (
+              <FormItem className="mt-8 w-[400px]">
+                <FormLabel>Risk Taking Capacity</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select am appropriate risk capacity" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="High">High</SelectItem>
+                      <SelectItem value="Medium">Medium</SelectItem>
+                      <SelectItem value="Low">Low</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <Button className="mt-2" disabled={isLoading} type="submit">
-              Submit
-            </Button>
-          </ScrollArea>
+          <Button className="mt-2" disabled={isLoading} type="submit">
+            Submit
+          </Button>
         </form>
       </Form>
     </>
--- lib/validations/AddUserDetailsFormValidation.ts
+++ lib/validations/AddUserDetailsFormValidation.ts
@@ -3,6 +3,6 @@ import * as z from "zod";
 export const AddUserDetailFormValidation = z.object({
   brokerName: z.string(),
   phoneNumber: z.number(),
-  investmentGoal: z.string(),
+  investmentGoal: z.number(),
   riskTakingCapacity: z.string(),
 });
